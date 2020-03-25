c4714178f96fee0f43bb01b9ae2c08b0adf1cdac
Gonzalo Gamez <takosaga@gmail.com>
2020-03-25 18:58:07 -0500

    Initial Commit

---
+++ HabitTracking/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HabitTracking
+//
+//  Created by Gonzalo Gamez on 3/25/20.
+//  Copyright © 2020 Gamez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
